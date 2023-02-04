f63d091c197ab687aa5423e52420a4efadebaa19
Jose Manuel Rosado <nepenteh@gmail.com>
2023-02-04 12:17:02 +0100

    Changes in UploadService interface and implementation

--- src/main/java/com/jmrh/app/models/services/UploadService.java
+++ src/main/java/com/jmrh/app/models/services/UploadService.java
@@ -9,15 +9,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface UploadService {
 
-	//Para cargar la imagen para verla
 	public Resource load(String filename) throws MalformedURLException;
-	
-	//Copiar una imagen en la carpeta del proyecto
 	public String copy(MultipartFile file) throws IOException;
-	
-	//Borrar una imagen de la carpeta del proyecto
 	public boolean delete(String filename);
-	
-	//Devuelve el path absoluto del fichero filename
 	public Path getPath(String filename);
 }
--- src/main/java/com/jmrh/app/models/services/UploadServiceImpl.java
+++ src/main/java/com/jmrh/app/models/services/UploadServiceImpl.java
@@ -15,41 +15,35 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class UploadServiceImpl implements UploadService {
-
-	// carpeta de imagenes subidas del proyecto
+	
 	final static String UPLOAD_FOLDER = "uploads";
 	
 	@Override
 	public Resource load(String filename) throws MalformedURLException {
 
-		// obtiene el path absoluto del archivo
-		Path pathArchivo = getPath(filename);
-		Resource recurso = null;
-		recurso = new UrlResource(pathArchivo.toUri()); // crea un recurso a partir del path
-		if (!recurso.exists() || !recurso.isReadable()) // comprueba si el recurso existe (el archivo está)
-			throw new RuntimeException("Error, no se puede cargar la imagen: " + pathArchivo.toString());
-		return recurso; // devuelve la imagen en forma de recursso
+		Path pathFile = getPath(filename);
+		Resource resource = null;
+		resource = new UrlResource(pathFile.toUri()); 
+		if (!resource.exists() || !resource.isReadable()) 
+			throw new RuntimeException("Error, no se puede cargar la imagen: " + pathFile.toString());
+		return resource; 
 	}
 
 	@Override
 	public String copy(MultipartFile file) throws IOException {
-		//genera un nombre único para el archivo
-		String nombreUnico = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
-		//genera un path absoluto para el archivo
-		Path pathUploadsAbsoluto = getPath(nombreUnico);
-		//hace una copia del fichero que viene por el formulario al camino absoluto
-		Files.copy(file.getInputStream(), pathUploadsAbsoluto);
+		String uniqueName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+		Path pathUploadsAbsolute = getPath(uniqueName);
+		Files.copy(file.getInputStream(), pathUploadsAbsolute);
 
-		return nombreUnico;
+		return uniqueName;
 	}
 
 	@Override
 	public boolean delete(String filename) {
-		//obtiene el path absoluto de la imagen
-		Path pathArchivo = getPath(filename);
-		File archivo = pathArchivo.toFile(); //convierto a objeto file
-		if(archivo.exists() && archivo.canRead()) {
-			if(archivo.delete()) {
+		Path pathFile = getPath(filename);
+		File file = pathFile.toFile(); 
+		if(file.exists() && file.canRead()) {
+			if(file.delete()) {
 				return true;
 			}
 		}
